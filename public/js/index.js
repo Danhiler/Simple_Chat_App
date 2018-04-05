1fdc55efdd223fdff8782c97ee7218166f5073c8
danhiler <danhiler@gmail.com>
2018-04-05 13:54:57 +0300

    sec

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,22 @@
 var socket = io();
+var userName="";
+
 socket.on('connect', () => {
 console.log("Connected to server")
 
 })
-socket.on('disconnect', () => {
+socket.on('disconnect', (userName) => {
 console.log("Disconnected to server")
+})
+socket.on('newUser',(message,users) => {
+  var li = jQuery('<li></li>')
+if(userName==="") {userName=users[users.length-1]}
+  li.text(`${message.from}: ${message.text}`)
+  jQuery('#messages').append(li)
+  jQuery('#users').append("You are "+userName+"<br><br>")
+  jQuery('#users').append(users.join("<br>"))
+
+
 })
 
 socket.on('newMessage',(message) => {
@@ -12,15 +24,17 @@ socket.on('newMessage',(message) => {
   var li = jQuery('<li></li>')
   li.text(`${message.from}: ${message.text}`)
   jQuery('#messages').append(li)
+
 })
 
 jQuery('#message-form').on('submit', function(e) {
 e.preventDefault();
 socket.emit('createMessage', {
-  from: "User",
+  from: userName,
   text: jQuery('[name=message]').val()
+
 }, function(e) {
-  console.log('message received')
+jQuery('[name=message]').val('')
 })
 }
 )
--- server/server.js
+++ server/server.js
@@ -9,20 +9,22 @@ var app = express();
 const port = process.env.PORT || 3000;
 var server = http.createServer(app);
 var io = socketIO(server);
+var users=[];
 
 app.use(express.static(publicPath));
 
 io.on('connection',(socket) => {
-  console.log('client is visting the app');
-
-socket.emit('newMessage',generateMessage('admin','Wellcome to the chat'))
-
-socket.broadcast.emit('newMessage',generateMessage('admin','New User Join the Chat'))
-
+  var userName="User"+Math.floor(Math.random()*100)
+  users.push(userName);
+socket.emit('newUser',generateMessage('admin','Wellcome to the chat'),users)
+socket.broadcast.emit('newUser',generateMessage('admin',`${userName} Joined the Chat`),users)
 
 
-  socket.on('disconnect', function () {
-    console.log('client is leaving the app');
+
+
+  socket.on('disconnect', function (userName) {
+  users.splice(users.find((user)=>user===userName),1)
+  socket.broadcast.emit('newUser',generateMessage('admin',`User${userName} left the Chat`),users)
     })
 
     socket.on('createMessage', (message,callback)=> {
